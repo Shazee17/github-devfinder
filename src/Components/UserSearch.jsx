134803bb6f276102c7509e27166793c490e67442
Shazee17 <shahzaman856.com>
2023-06-30 12:34:04 +0500

    made some changes

--- src/Components/UserSearch.jsx
+++ src/Components/UserSearch.jsx
@@ -2,14 +2,25 @@ import React, { useState, useEffect } from 'react';
 import DisplayUser from './DisplayUser';
 
 const UserSearch = () => {
-  const [userData, setUserData] = useState([]);
+  const [userData, setUserData] = useState(null);
   const [searchTerm, setSearchTerm] = useState('Shazee17'); // Default username
+  const [error, setError] = useState(false);
 
   const getData = () => {
     fetch(`https://api.github.com/users/${searchTerm}`)
-      .then((response) => response.json())
+      .then((response) => {
+        if (response.ok) {
+          return response.json();
+        } else {
+          throw new Error('User not found');
+        }
+      })
       .then((data) => {
         setUserData(data);
+        setError(false);
+      })
+      .catch((error) => {
+        setError(true);
       });
   };
 
@@ -19,27 +30,28 @@ const UserSearch = () => {
   };
 
   useEffect(() => {
-    setSearchTerm(''); 
-    getData(); 
+    setSearchTerm('');
+    getData();
   }, []);
-  
+
   return (
     <>
-    
-    <div>
-      <form onSubmit={handleSubmit} className="searchForm">
-        <input
-          type="text"
-          placeholder="Enter GitHub username"
-          value={searchTerm}
-          onChange={(e) => setSearchTerm(e.target.value)}
-        />
-        <button type="submit">Search</button>
-      </form>
+      <div>
+        <form onSubmit={handleSubmit} className="searchForm">
+          <input
+            type="text"
+            placeholder="Enter GitHub username"
+            value={searchTerm}
+            onChange={(e) => setSearchTerm(e.target.value)}
+          />
+          <button type="submit">Search</button>
+        </form>
+      </div>
 
-      
-    </div>
-    <DisplayUser user={userData} />
+      {error && <p style={{
+        color: "White"
+      }}>User doesn't exist.</p>}
+      {!error && userData && <DisplayUser user={userData} />}
     </>
   );
 };
